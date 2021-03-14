bfe67f3eeda9c613e2a1b73543bd029a08e1c5fe
walid <walid@192.168.1.105>
2021-03-14 09:39:56 +0000

    my second commit

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>15e8c053-5721-4cd5-97ae-b1dcb7f23cff</testSuiteGuid>
+   <testCaseLink>
+      <guid>905fb511-acaf-4ccf-abfd-789cc8dc7571</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/checkConection</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
